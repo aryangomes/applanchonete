ed77e047079dbc9079e5e6693748a36bb62cf6a4
Aryan <aryandantasgomes@hotmail.com>
2016-02-09 12:01:58 -0300

    Adicionados arquivos de inserção das permissões de usuário

---
+++ inserts-auth-item-permissoes.sql
@@ -0,0 +1,43 @@
+INSERT INTO `applanchonete`.`auth_item` (`name`, `type`, `description`) 
+
+VALUES ('despesa', 2, 'Despesa'),
+('index-despesa', 3, 'Listar Despesas'),
+('view-despesa', 4, 'Visualizar Despesa'),
+('create-despesa', 5, 'Criar Despesa'),
+('update-despesa', 6, 'Editar Despesa'),
+('delete-despesa', 7, 'Deletar Despesa'),
+
+('caixa', 8, 'Caixa'),
+('index-caixa', 9, 'Listar Caixas'),
+('view-caixa', 10, 'Visualizar Caixa'),
+('create-caixa', 11, 'Criar Caixa'),
+('update-caixa', 12, 'Editar Caixa'),
+('delete-caixa', 13, 'Deletar Caixa'),
+
+('fornecedor', 14, 'Fornecedor'),
+('index-fornecedor', 15, 'Listar Fornecedores'),
+('view-fornecedor', 16, 'Visualizar Fornecedor'),
+('create-fornecedor', 17, 'Criar Fornecedor'),
+('update-fornecedor', 18, 'Editar Fornecedor'),
+('delete-fornecedor', 19, 'Deletar Fornecedor'),
+
+('relatorio', 20, 'Relatório'),
+('index-relatorio', 21, 'Listar Relatórios'),
+('view-relatorio', 22, 'Visualizar Relatório'),
+('create-relatorio', 23, 'Criar Relatório'),
+('update-relatorio', 24, 'Editar Relatório'),
+('delete-relatorio', 25, 'Deletar Relatório'),
+
+('compra', 26, 'Compra'),
+('index-compra', 27, 'Listar Compras'),
+('view-compra', 28, 'Visualizar Compra'),
+('create-compra', 29, 'Criar Compra'),
+('update-compra', 30, 'Editar Compra'),
+('delete-compra', 31, 'Deletar Compra'),
+
+('user', 32, 'Usuário'),
+('index-user', 33, 'Listar Usuários'),
+('view-user', 34, 'Visualizar Usuário'),
+('create-user', 35, 'Criar Usuário'),
+('update-user', 36, 'Editar Usuário'),
+('delete-user', 37, 'Deletar Usuário');

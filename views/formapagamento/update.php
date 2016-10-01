20735d0eb8aef1fcbc315bfea2d45d88627b7528
Aryan <aryan.gomes@gmail.com>
2016-10-01 00:57:56 -0300

    Complemento do commit anterior

--- views/formapagamento/update.php
+++ views/formapagamento/update.php
@@ -6,10 +6,10 @@ use yii\helpers\Html;
 /* @var $modelFormapagamento app\models\Formapagamento */
 
 $this->title = Yii::t('app', 'Update {modelClass}: ', [
-    'modelClass' => 'Formapagamento',
-]) . $modelFormapagamento->idTipoPagamento;
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Formapagamentos'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $modelFormapagamento->idTipoPagamento, 'url' => ['view', 'id' => $modelFormapagamento->idTipoPagamento]];
+    'modelClass' => 'Forma de Pagamento',
+]) .': '. $modelFormapagamento->titulo;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Forma de Pagamentos'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $modelFormapagamento->titulo, 'url' => ['view', 'id' => $modelFormapagamento->idTipoPagamento]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
 <div class="formapagamento-update">
